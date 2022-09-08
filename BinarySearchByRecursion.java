f5c15f7793ae5167bb1fcbe884f5abc7ac49637c
Mayank Gupta <43218169+mayank0707@users.noreply.github.com>
2022-09-08 22:57:59 +0530

    Day 17
    Completed Binary Search by Recursion in Java. 
    Also practiced some questions on Recursion concept on leetcode.

---
+++ BinarySearchByRecursion.java
@@ -0,0 +1,23 @@
+
+public class BinarySearchByRecursion {
+
+	public static void main(String[] args) {
+		int[] arr= {1,5,8,10,20,25,30,50};
+		int target=25;
+		System.out.println(search(arr,target,0,arr.length-1));
+	}
+	
+	static int search(int[] arr, int target, int start, int end) {
+		if(start>end) {
+			return -1;
+		}
+		int mid=start+(end-start)/2;
+		if(arr[mid]==target) {
+			return mid;
+		}
+		else if(target<arr[mid]) {
+			return search(arr,target,start,mid-1);
+		}
+		return search(arr, target, mid+1,end);
+	}
+}
