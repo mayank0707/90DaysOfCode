4d9c0c99ec3d0480c0fb55c9a0041d6ee56dd020
Mayank Gupta <43218169+mayank0707@users.noreply.github.com>
2022-10-30 16:07:34 +0530

    Day 64 leetcode problem
    Solved this question using HashMap concept.
    P.s. Yesterday, I forgot to post the progress.

---
+++ sumOfUnique.java
@@ -0,0 +1,28 @@
+Leetcode problem- https://leetcode.com/problems/sum-of-unique-elements/
+
+Q.) You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+    Return the sum of all the unique elements of nums.
+  
+Solution-
+  
+  public int sumOfUnique(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        for(int n:nums){
+            if(!map.containsKey(n)){
+                map.put(n,0);
+            }
+            map.put(n, map.get(n) + 1);
+        }
+        System.out.println("HashMap: " + map);
+
+    Integer value=1;
+    // iterate each entry of hashmap
+     for(Map.Entry m : map.entrySet()){    
+       if(m.getValue() == value) {
+         sum+=(Integer)m.getKey();
+       }
+     }
+    return sum;
+        
+ }
